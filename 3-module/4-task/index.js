1bc0b138a38effe95ab0184bb7f7153830ac0dbc
Kira <kira-ismailova@bk.ru>
2023-09-27 21:04:55 +0300

    fix

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-function showSalary(users, age) {
-  let str = '';
-  str = users
-  .filter(user => user.age <= age)
-  .map(user => `${user.name}, ${user.balance}`)
-  .join('\n');
-  return str;
-=======
+
 function showSalary(data, age) {
   return data
     .filter(item => item.age <= age)
     .map(item => `${item.name}, ${item.balance}`)
     .join('\n');
->>>>>>> 214bfb2fb52dc51f07f69126193420a1a1d6d209
 }
